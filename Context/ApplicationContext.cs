736d349ecb0457084978cad6948d50bd12a63a1e
s24493 <s24493@pjwstk.edu.pl>
2024-06-04 20:47:35 +0200

    Added database context

---
+++ Context/ApplicationContext.cs
@@ -0,0 +1,44 @@
+using MedicalAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalAPI.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Medicament> Medicaments { get; set; }
+        public DbSet<Prescription> Prescriptions { get; set; }
+        public DbSet<PrescriptionMedicament> PrescriptionMedicaments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<PrescriptionMedicament>()
+                .HasKey(pm => new { pm.IdMedicament, pm.IdPrescription });
+
+            modelBuilder.Entity<Prescription>()
+                .HasOne(p => p.Doctor)
+                .WithMany(d => d.Prescriptions)
+                .HasForeignKey(p => p.IdDoctor);
+
+            modelBuilder.Entity<Prescription>()
+                .HasOne(p => p.Patient)
+                .WithMany(pt => pt.Prescriptions)
+                .HasForeignKey(p => p.IdPatient);
+
+            modelBuilder.Entity<PrescriptionMedicament>()
+                .HasOne(pm => pm.Prescription)
+                .WithMany(p => p.PrescriptionMedicaments)
+                .HasForeignKey(pm => pm.IdPrescription);
+
+            modelBuilder.Entity<PrescriptionMedicament>()
+                .HasOne(pm => pm.Medicament)
+                .WithMany(m => m.PrescriptionMedicaments)
+                .HasForeignKey(pm => pm.IdMedicament);
+        }
+    }
+}
